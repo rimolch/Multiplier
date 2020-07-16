e39c939e3eeb3a339c4390f0f3dd75b40762a46b
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-07-16 13:20:43 +0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
+function makeMultiplier(multiplier){
+  function b(){
+      console.log("Multiplier is:" + multiplier);    
+    }
+ b();
+ return(
+    function(x){
+        return multiplier = x;
+    }
+    
+    );
+}
+var doubleAll=makeMultiplier(2);
+console.log(doubleAll(10));
 
